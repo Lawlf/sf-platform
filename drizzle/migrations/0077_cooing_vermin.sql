b865e039b7269407da2642707d41ae6ffabce7a0
Arthur Fernandes <torofote@gmail.com>
2026-06-24 23:07:36 -0300

    chore(migrations): remove orphaned 0077 from pre-refactor user columns

--- drizzle/migrations/0077_cooing_vermin.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "checklist_debt_dismissed_at" timestamp with time zone;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "checklist_goal_dismissed_at" timestamp with time zone;
